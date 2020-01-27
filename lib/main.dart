09da6bed832173ba71ee8b16464b019bdc581780
Virjanand <virjanand@virjanand.com>
2020-01-27 06:18:10 +0100

    When reaching the end of the questions, show popup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,17 +31,27 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-
-  int questionNumber = 0;
-
-  void getNewQuestion() {
+  void checkAnswer(bool userPickedAnswer) {
     setState(() {
-      questionNumber = (questionNumber + 1) % 3;
+      if (userPickedAnswer == quizBrain.getQuestionAnswer()) {
+        scoreKeeper.add(
+          Icon(Icons.check, color: Colors.green),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(Icons.close, color: Colors.red),
+        );
+      }
+      quizBrain.nextQuestion();
+      if (quizBrain.isFinished()) {
+        Alert(
+                context: context,
+                title: "End of Questions",
+                desc: "You've reached the end of the quiz")
+            .show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
     });
   }
 
@@ -53,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +92,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                getNewQuestion();
+                checkAnswer(true);
               },
             ),
           ),
@@ -97,7 +111,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                getNewQuestion();
+                checkAnswer(false);
               },
             ),
           ),
